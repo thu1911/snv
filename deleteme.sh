676d9a0bb496f850b7866b2c08bfafcb466b85df
tx <1019363681@qq.com>
2019-12-19 22:50:07 +0800

    everything works

--- deleteme.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-location="/data8t/mtx/scSNV/kim_data/data/fastq/"
-for i in `find $location -maxdepth 1 -type d`;
-do
-    if [ -d $i ];then
-        echo $i | awk -F/ '{print $NF}'
-    fi
-done
-
